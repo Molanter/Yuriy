69e723e0b8282ebf75fb688ac796ccd0db5a698d
Molanter <62011484+Molanter@users.noreply.github.com>
2022-04-12 22:42:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let myname = 1;
 const yourname = prompt('If you see your name (more down) enter your number!!!\n\nVisiters:\n1.Suzana\n2.Yuran\n3.Adrey\n4.Andrey\n5.Ne ya...\nWhat is you name ?');
 
 if ( myname == yourname ) {
-  alert(`${name} You Are The BEST For Me)))`);
+  alert(` You Are The BEST For Me)))`);
   endsWith();
 } if ( yura == yourname ) {
-  alert(`Privet ${name}! I know that you are loh) Just joke)Do not cry pls!)`);
+  alert(`Privet ! I know that you are loh) Just joke)Do not cry pls!)`);
 
 } else {
-  alert(`${name} Sorry...( I am not looking for you(`);
+  alert(` Sorry...( I am not looking for you(`);
 }
